5291cf6b35c38ab6ca74c0908b4da05d93f8e539
herzschaltung <herzschaltung@gmail.com>
2012-03-18 17:26:00 +0000

    Now the main form displays version and revision in the title.

--- solution/PackageManagerForm.cs
+++ solution/PackageManagerForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
 
@@ -14,6 +15,17 @@ namespace msile
         public PackageManagerForm()
         {
             InitializeComponent();
+        }
+
+        private void LoadHandler(object sender, EventArgs e)
+        {
+            UpdateTitle();
+        }
+
+        private void UpdateTitle()
+        {
+            Version version = Assembly.GetExecutingAssembly().GetName().Version;
+            this.Text = String.Format("Msile Package Manager v{0}.{1} Alpha r{2}", version.Major, version.Minor, version.Build);
         }
     }
 }
