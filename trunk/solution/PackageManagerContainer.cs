9f0f9e0f6e726c69d6e15129dbd273911a821248
herzschaltung@gmail.com <herzschaltung@gmail.com@50bb8526-6067-d47c-6d6a-f8e112d61b2b>
2012-03-26 02:01:32 +0000

    Fixed violations of StyleCop rules Sa1200 and SA1210 regarding using directives.

--- trunk/solution/PackageManagerContainer.cs
+++ trunk/solution/PackageManagerContainer.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
+﻿
 
 namespace Msile
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Windows.Forms;
+
     /// <summary>
     /// A general container for the PackageManager form.
     /// </summary>
