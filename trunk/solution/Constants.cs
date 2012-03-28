3a6487cdcc0006b277496f0bf4770ccbb0d2f624
herzschaltung@gmail.com <herzschaltung@gmail.com@50bb8526-6067-d47c-6d6a-f8e112d61b2b>
2012-03-28 20:09:53 +0000

    Added compositing to the form via CreateParams. This means the whole thing is now double-buffered, including child controls.

---
+++ trunk/solution/Constants.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Constants.cs" company="Camilo Martin">
+//     Licensed under the GNU GPL v3. 
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Msile
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// Holds common constants.
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// With this style turned on for your form, Windows does double-buffering on the form and all its child controls.
+        /// </summary>
+        public const int WS_EX_COMPOSITED = 0x02000000;
+    }
+}
