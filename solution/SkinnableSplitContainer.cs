29ea2eef6b34de15c59d4cd4b1393c18a03ee698
herzschaltung <herzschaltung@gmail.com>
2012-03-28 14:35:20 +0000

    Created a skinnable version of a SplitContainer.

---
+++ solution/SkinnableSplitContainer.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SkinnableSplitContainer.cs" company="Camilo Martin">
+//     Licensed under the GNU GPL v3. 
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Msile
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// A skinnable version of the SplitContainer.
+    /// </summary>
+    public class SkinnableSplitContainer : SplitContainer
+    {
+    }
+}
