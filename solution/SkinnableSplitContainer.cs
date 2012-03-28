8a197b061bf9442ae8fedc877a895bcd67f38ff0
herzschaltung <herzschaltung@gmail.com>
2012-03-28 18:26:42 +0000

    Added custom drawing code to the SkinnableSplitContainer, and methods to get the head, body and tail rectangles, plus an example to see it working.

--- solution/SkinnableSplitContainer.cs
+++ solution/SkinnableSplitContainer.cs
@@ -8,6 +8,7 @@ namespace Msile
 {
     using System;
     using System.Collections.Generic;
+    using System.Drawing;
     using System.Linq;
     using System.Text;
     using System.Windows.Forms;
@@ -17,5 +18,60 @@ namespace Msile
     /// </summary>
     public class SkinnableSplitContainer : SplitContainer
     {
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            // Just to show that it works.
+            g.FillRegion(Brushes.Blue, new Region(this.getSplitterBodyRectangle()));
+            g.FillRegion(Brushes.Red, new Region(this.getSplitterHeadRectangle()));
+            g.FillRegion(Brushes.Lime, new Region(this.getSplitterTailRectangle()));
+        }
+
+        private Rectangle getSplitterHeadRectangle()
+        {
+            Rectangle result = SplitterRectangle;
+            if (this.Orientation == Orientation.Horizontal)
+            {
+                result.Width = result.Height;
+            }
+            else
+            {
+                result.Height = result.Width;
+            }
+            return result;
+        }
+
+        private Rectangle getSplitterBodyRectangle()
+        {
+            Rectangle result = SplitterRectangle;
+            if (this.Orientation == Orientation.Horizontal)
+            {
+                result.X = result.Height;
+                result.Width -= result.Height * 2;
+            }
+            else
+            {
+                result.Y = result.Width;
+                result.Height -= result.Width * 2;
+            }
+            return result;
+        }
+
+        private Rectangle getSplitterTailRectangle()
+        {
+            Rectangle result = SplitterRectangle;
+            if (this.Orientation == Orientation.Horizontal)
+            {
+                result.X = result.Width - result.Height;
+                result.Width = result.Height;
+            }
+            else
+            {
+                result.Y = result.Height - result.Width;
+                result.Height = result.Width;
+            }
+            return result;
+        }
     }
 }
