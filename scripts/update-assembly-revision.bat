3889e0e50f6626237d1bfe9cab4f40120dfeb406
herzschaltung <herzschaltung@gmail.com>
2012-03-31 16:51:55 +0000

    Edited the update-assembly-revision.bat pre-commit hook script: now it will update AssemblyInfo.cs files below the current directory (and sub-directories) so that if you're committing the wiki directory, you won't update the AssemblyInfo in the trunk. Also, this will work better when we have something in the tags and branches.

--- scripts/update-assembly-revision.bat
+++ scripts/update-assembly-revision.bat
@@ -6,11 +6,18 @@ REM Store current directory, go to where the script is.
 FOR /F "TOKENS=*" %%D IN ('CD') DO SET PWD=%%D
 CD /D %~DP0
 
-REM Get the HEAD revision number, increment it and place it in the AssemblyInfo.
+REM Get the HEAD revision number, increment and it.
 FOR /F "TOKENS=*" %%R IN ('get-head-revision.bat') DO SET REVISION=%%R
 SET /A REVISION+=1 > NUL
-..\TOOLS\SED.EXE -i -r "s/^\[assembly: AssemblyVersion\(\"([0-9]+)\.([0-9]+)\.[0-9]+\"\)\]$/\[assembly: AssemblyVersion\(\"\1\.\2\.%REVISION%\"\)\]/g" ..\solution\Properties\AssemblyInfo.cs
-..\TOOLS\SED.EXE -i -r "s/^\[assembly: AssemblyFileVersion\(\"([0-9]+)\.([0-9]+)\.[0-9]+\"\)\]$/\[assembly: AssemblyFileVersion\(\"\1\.\2\.%REVISION%\"\)\]/g" ..\solution\Properties\AssemblyInfo.cs
+
+REM Update each AssemblyInfo.cs found in whatever directory (and subdirectories) you're checking in.
+REM Please do note that AssemblyInfo.cs must be accessible BELOW whatever you're checking in.
+REM So if you are checking in files in a directory below the solution this WON'T WORK.
+REM TL;DR: use the "SVN Commit" context menu entry in the solution directory or a file in it, not in a subdirectory.
+FOR /F "TOKENS=*" %%A IN ('DIR /B /S %3\*AssemblyInfo.cs 2^> NUL') DO (
+	..\TOOLS\SED.EXE -i -r "s/^\[assembly: AssemblyVersion\(\"([0-9]+^)\.([0-9]+^)\.[0-9]+\"\)\]$/\[assembly: AssemblyVersion\(\"\1\.\2\.%REVISION%\"\)\]/g" "%%A"
+	..\TOOLS\SED.EXE -i -r "s/^\[assembly: AssemblyFileVersion\(\"([0-9]+^)\.([0-9]+^)\.[0-9]+\"\)\]$/\[assembly: AssemblyFileVersion\(\"\1\.\2\.%REVISION%\"\)\]/g" "%%A"
+)
 
 REM Clean up because this port of sed poops a temp file in the directory where you are.
 DEL sed*
